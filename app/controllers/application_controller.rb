a8b12532b9b754bdc37c2118ac25a9820a4ee9df
keisukekodama <myzk.kodama@gmail.com>
2019-03-26 17:37:42 +0900

    Basic4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
 
   private
   def production?

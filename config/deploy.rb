13e9dcc0860bbbfe5486c70fd30e78665a8c2c58
keisukekodama <myzk.kodama@gmail.com>
2019-03-26 14:56:21 +0900

    Basic2

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,15 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
+  username: ENV["BASIC_AUTH_USER"],
+  password: ENV["BASIC_AUTH_PASSWORD"]
+}
+
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/merukari.pem']
 
